58e57dedd8592e53fe377b3e1f9b56279d5a56f7
LongNguyen31 <longnmp31@gmail.com>
2022-03-28 16:51:40 +0700

    Add image coding for future is working

--- app/src/main/java/com/longnp/lnsocial/presentation/main/MainActivity.kt
+++ app/src/main/java/com/longnp/lnsocial/presentation/main/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import androidx.core.content.ContextCompat
